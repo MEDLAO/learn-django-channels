1ffa795e2ab00d4fc0e5194ea6731412ceab1744
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-11-14 18:47:06 +0100

    Configure ASGI routing for WebSocket support

--- learn_django_channels/asgi.py
+++ learn_django_channels/asgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+from chat.consumers import ChatConsumer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learn_django_channels.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learn_django_channels.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": URLRouter([
+        path("ws/chat/", ChatConsumer.as_asgi()),
+    ]),
+})
+
