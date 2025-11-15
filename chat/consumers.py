ec0a69450d44779f258f3cc98495bf1df5bbfc56
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-11-15 18:19:56 +0100

    Add basic ChatConsumer for WebSocket handling

---
+++ chat/consumers.py
@@ -0,0 +1,17 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        # Accept the WebSocket connection
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Called when the socket closes
+        pass
+
+    async def receive(self, text_data):
+        # Echo back whatever message the client sent
+        await self.sent(text_data=text_data)
+
