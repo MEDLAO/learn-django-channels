4a0880dd87ac927b527104257cf6abb19ce7e23a
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-11-22 19:33:41 +0100

    Use /ws/chat/room1/ in frontend WebSocket URL for simple room test

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,7 +5,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.groupe_name = f"chat_{self.room_name}"
+        self.group_name = f"chat_{self.room_name}"
 
         await self.channel_layer.group_add(
             self.group_name,
