a0379eec6635fda58310a40340700675119ce07e
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-11-26 21:22:52 +0100

    add paths for login and logout

--- learn_django_channels/urls.py
+++ learn_django_channels/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Login and logout
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+    # Chat page
     path("", TemplateView.as_view(template_name="chat.html")),
 ]
